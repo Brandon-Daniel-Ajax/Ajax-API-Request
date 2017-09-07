e419ae63ea661d10052154449a94de44df04ae55
brandongossen <brandongossen@yahoo.com>
2017-09-07 11:52:41 -0500

    Create Layout
    -Create the layout for the three day weather forecast in javascript
    
    -Round the temperature and add a degree symbol

--- js/weather_map.js
+++ js/weather_map.js
@@ -11,10 +11,19 @@
         }).done(function (data) {
             console.log(data);
             var html1 = "";
-            html1 += "<h2>" + data.list[0].main.temp_max + "/" + data.list[0].main.temp_min + "</h2>" +
-                "<p><strong>" + data.list[0].weather[0].main + ": </strong>" + data.list[0].weather[0].description + "</p>"
-            console.log(html1);
+            html1 += "<h2>" + Math.round(data.list[0].main.temp_max) + "/" + Math.round(data.list[0].main.temp_min) + "&#8457;</h2>" +
+                "<p><strong>" + data.list[0].weather[0].main + ": </strong>" + data.list[0].weather[0].description + "</p><p><strong>" + "Humidity: </strong>" + data.list[0].main.humidity + "</p><p><strong>" + "Wind: </strong>" + data.list[0].wind.speed + "</p><p><strong>" + "Pressure: </strong>" + data.list[0].main.pressure + "</p>";
             $("#div1").append(html1);
+
+            var html2 = "";
+            html2 += "<h2>" + Math.round(data.list[1].main.temp_max) + "/" + Math.round(data.list[1].main.temp_min) + "&#8457;</h2>" +
+                "<p><strong>" + data.list[1].weather[0].main + ": </strong>" + data.list[1].weather[0].description + "</p><p><strong>" + "Humidity: </strong>" + data.list[1].main.humidity + "</p><p><strong>" + "Wind: </strong>" + data.list[1].wind.speed + "</p><p><strong>" + "Pressure: </strong>" + data.list[1].main.pressure + "</p>";
+            $("#div2").append(html2);
+
+            var html3 = "";
+            html3 += "<h2>" + Math.round(data.list[2].main.temp_max) + "/" + Math.round(data.list[2].main.temp_min) + "&#8457;</h2>" +
+                "<p><strong>" + data.list[2].weather[0].main + ": </strong>" + data.list[2].weather[0].description + "</p><p><strong>" + "Humidity: </strong>" + data.list[2].main.humidity + "</p><p><strong>" + "Wind: </strong>" + data.list[2].wind.speed + "</p><p><strong>" + "Pressure: </strong>" + data.list[2].main.pressure + "</p>";
+            $("#div3").append(html3);
         });
     });
 })();
