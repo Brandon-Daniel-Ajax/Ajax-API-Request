a129970570d0ed1e41a400de4a0ea690be5b9910
brandongossen <brandongossen@yahoo.com>
2017-09-07 16:48:00 -0500

    Old code taken out
    -Took out old colde that was refactored

--- js/weather_map.js
+++ js/weather_map.js
@@ -22,16 +22,6 @@
         }
 
         function getWeather(lat, lon) {
-            // $.get('http://api.openweathermap.org/data/2.5/weather', {
-            //     APPID: "1d9259fc21b4fb3a6976934537609e66",
-            //     lat: lat,
-            //     lon: lon
-            // }).done(function (data) {
-            //     var city = data.name;
-            //     var html = "";
-            //     html += "<h2>" + city + "</h2>";
-            //     $("#coordinatesCity").append(html);
-            // });
             $.get('http://api.openweathermap.org/data/2.5/forecast', {
                 APPID: "1d9259fc21b4fb3a6976934537609e66",
                 lat: lat,
@@ -54,22 +44,6 @@
                         "</span><p><strong>" + day.weather[0].main + ": </strong>" + day.weather[0].description + "</p><p><strong>" + "Humidity: </strong>" + day.main.humidity + "</p><p><strong>" + "Wind: </strong>" + day.wind.speed + "</p><p><strong>" + "Pressure: </strong>" + day.main.pressure + "</p></div>";
                     $("#forecasts").append(html);
                 })
-
-                // var icon1 =  data.list[1].weather[0].icon;
-                // var url1 = "http://openweathermap.org/img/w/" + icon1 + ".png";
-                // var img1 = "<img src='" + url1 + "'>";
-                // var html2 = "";
-                // html2 += "<h2>" + Math.round(data.list[1].main.temp_max) + "/" + Math.round(data.list[1].main.temp_min) + "&#8457;</h2><span>" + img1 +
-                //     "</span><p><strong>" + data.list[1].weather[0].main + ": </strong>" + data.list[1].weather[0].description + "</p><p><strong>" + "Humidity: </strong>" + data.list[1].main.humidity + "</p><p><strong>" + "Wind: </strong>" + data.list[1].wind.speed + "</p><p><strong>" + "Pressure: </strong>" + data.list[1].main.pressure + "</p>";
-                // $("#div2").append(html2);
-                //
-                // var icon2 =  data.list[2].weather[0].icon;
-                // var url2 = "http://openweathermap.org/img/w/" + icon2 + ".png";
-                // var img2 = "<img src='" + url2 + "'>";
-                // var html3 = "";
-                // html3 += "<h2>" + Math.round(data.list[2].main.temp_max) + "/" + Math.round(data.list[2].main.temp_min) + "&#8457;</h2><span>" + img2 +
-                //     "</span><p><strong>" + data.list[2].weather[0].main + ": </strong>" + data.list[2].weather[0].description + "</p><p><strong>" + "Humidity: </strong>" + data.list[2].main.humidity + "</p><p><strong>" + "Wind: </strong>" + data.list[2].wind.speed + "</p><p><strong>" + "Pressure: </strong>" + data.list[2].main.pressure + "</p>";
-                // $("#div3").append(html3);
             });
         }
     })
